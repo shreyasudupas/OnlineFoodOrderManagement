4d3c1c1ad5a97439376590aa3e13825f8936c8ec
Shreyas Udupa <shreyasudupas@gmail.com>
2021-09-09 16:58:44 +0530

    Add Authentication MenuMicroservice

--- MenuInventory.Microservice/Installers/AuthenticationInstaller.cs
+++ MenuInventory.Microservice/Installers/AuthenticationInstaller.cs
@@ -1,8 +1,6 @@
-﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+﻿using Common.Utility.Security;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
 
 namespace MenuInventory.Microservice.Installers
 {
@@ -20,25 +18,8 @@ namespace MenuInventory.Microservice.Installers
             //    options.Authority = $"https://{configuration["Auth0:Domain"]}/";
             //    options.Audience = configuration["Auth0:Audience"];
             //});
-            var token = configuration["Jwt:Secret"];
 
-            services.AddAuthentication(options =>
-            {
-                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            }).AddJwtBearer(options =>
-            {
-                options.SaveToken = true;
-                options.TokenValidationParameters = new TokenValidationParameters
-                {
-                    ValidateIssuerSigningKey = true,
-                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(token)),
-                    ValidateIssuer = true,
-                    ValidateAudience = true
-                };
-                options.Authority = $"https://{configuration["Auth0:Domain"]}/";
-                options.Audience = configuration["Auth0:Audience"];
-            });
+            services.AddJwtAuthentication(configuration);
         }
     }
 }
