46c282008fc3e4b9ce351c09928287fb0d04b4d0
Shreyas Udupa <shreyasudupas@gmail.com>
2021-10-01 17:43:54 +0530

    Additiong things added

--- MenuOrder.MicroService/Startup.cs
+++ MenuOrder.MicroService/Startup.cs
@@ -1,17 +1,11 @@
+using HealthChecks.UI.Client;
 using MenuOrder.MicroService.Extensions;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace MenuOrder.MicroService
 {
@@ -60,6 +54,11 @@ namespace MenuOrder.MicroService
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapHealthChecks("hc", new HealthCheckOptions
+                {
+                    Predicate = _ => true,
+                    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+                });
                 endpoints.MapControllers();
             });
         }
