94ea560284232e5c54ff00e5e180f6a0ff3f0dac
Shreyas Udupa <shreyas.udupa@siemens.com>
2023-03-07 15:52:11 +0530

    Adding task to Microservice

---
+++ WebJobs/VendorRegistration.WebJob/Program.cs
@@ -0,0 +1,10 @@
+using VendorRegistration.WebJob;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
