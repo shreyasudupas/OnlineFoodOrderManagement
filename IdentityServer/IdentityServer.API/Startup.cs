e8dc3101063bdf4710bc7ece4b506716297808a4
Shreyas Udupa <shreyas.udupa@siemens.com>
2023-08-30 16:03:45 +0530

    IDS image path

--- IdentityServer/IdentityServer.API/Startup.cs
+++ IdentityServer/IdentityServer.API/Startup.cs
@@ -20,10 +20,12 @@ using MenuOrder.Shared.Services.Interfaces;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
@@ -31,6 +33,7 @@ using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
@@ -194,6 +197,13 @@ namespace IdentityServer.API
             }
 
             app.UseStaticFiles();
+
+            app.UseStaticFiles(new StaticFileOptions()
+            {
+                FileProvider = new PhysicalFileProvider(
+                            Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", @"images")),
+                RequestPath = new PathString("/ids-images")
+            });
 
             app.UseCors("IdentityReactSpa.Cors");
 
